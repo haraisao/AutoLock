c7bc4375dc27d0cd053b984a7439cfa7a4552768
Isao Hara <haraisao@gmail.com>
2017-04-28 07:59:51 +0000

    Init commit

---
+++ autolock.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+import sys
+import time
+import wiringpi
+
+import nfc
+from binascii import hexlify
+
+#
+#  Servo Motor Control
+#
+alreadyInit = False
+
+class ServoMotor:
+  def __init__(self):
+    self.readyGpio = alreadyInit
+    self.PWM_PIN=[18, 13]
+    self.pwm_range = 1920
+    self.pwm_clock = 200
+
+  def initGpio(self):
+    if self.readyGpio == False:
+      wiringpi.wiringPiSetupGpio()
+      self.readyGpio = True
+
+  def setup(self,id):
+    self.initGpio()
+    if id in self.PWM_PIN:
+      self.setupPwm(id)
+    else:
+     self.setupSoftPwm(id)
+
+  def setupPwm(self, id):
+    wiringpi.pinMode(id, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+    wiringpi.pwmSetRange(self.pwm_range)
+    wiringpi.pwmSetClock(self.pwm_clock)
+
+  def setupSoftPwm(self,id):
+    wiringpi.pinMode(id, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.softPwmCreate(id, 0, self.pwm_clock/2)
+
+  def pwmWrite(self, id, angle):
+    if id in self.PWM_PIN:
+      wiringpi.pwmWrite(id, angle)
+    else:
+      wiringpi.softPwmWrite(id, angle/10)
+
+  def rotate(self, id, angle):
+    self.pwmWrite(id, angle)
+    if angle > 0:
+      time.sleep(0.6)
+      self.pwmWrite(id, 0)
+
+  def motor_main(id, angle):
+    motor = ServoMotor()
+    motor.setup(id)
+    motor.rotate(id, angle)
+
+#
+#  NFC
+#
+class NfcReader:
+  def __init__(self):
+    self.open_device()
+    self.registeredCards = []
+    self.clear_id()
+
+  def open_device(self, typ='usb'):
+    try:
+      self.clf = nfc.ContactlessFrontend(typ)
+    except:
+      self.clf = None
+      
+  def check_services(self, tag, start, n):
+    services = [nfc.tag.tt3.ServiceCode(i >> 6, i & 0x3f)
+                for i in xrange(start, start+n)]
+    versions = tag.request_service(services)
+    for i in xrange(n):
+        if versions[i] == 0xffff: continue
+        print services[i], versions[i]
+
+  def get_id(self, tag):
+    return tag.identifier.encode("hex")
+   
+  def register_card(self, tag):
+    id = self.get_id(tag)
+    if id in self.registeredCards :
+      print "Already registered : %s" % id
+    else:
+      self.registeredCards.append(id)
+      print "Register card : %s" % id
+
+  def check_card(self, tag):
+    id = self.get_id(tag)
+    if id in self.registeredCards :
+      self.callback['check_card']
+    else:
+      self.registeredCards.append(id)
+
+  def print_tag_info(self, tag):
+    print tag.type
+    print tag._product
+    print hexlify(tag.pmm)
+    print self.get_id(tag)
+   
+  def show_tag_info(self, tag):
+    print tag
+    services = tag.dump()
+    for x in services:
+      print x
+
+  def show_syscode(self, tag):
+    system_codes = tag.request_system_code()
+    print "%d system code found." % len(system_codes)
+    print system_codes
+
+  def show_all_services(self, tag):
+    print tag
+    n = 32
+    for i in xrange(0, 0x10000, n):
+        self.check_services(tag, i, n)
+
+  def call(self, func):
+    if self.clf:
+      self.clf.connect(rdwr={'on-connect': func})
+    else:
+      print "ERROR: no NFC reader"
+    
+  def read_syscode(self):
+    self.call(self.show_syscode)
+      
+  def dump_tag(self):
+    self.call(self.show_tag_info)
+
+  def info(self):
+    if self.clf:
+      self.clf.connect(rdwr={'on-connect': self.save_id})
+
+  def save_id(self, tag):
+    self.current_card_id=self.get_id(tag)
+
+  def clear_id(self):
+    self.current_card_id=None
+
+  def is_registered(self):
+    res=False
+    if self.current_card_id in self.registeredCards:
+       res=True
+    self.clear_id()
+    return res
+
+#
+#
+# 
+class Lock:
+  def __init__(self, pin=18):
+    self.motor_pin = pin
+    self.red = 24
+    self.green = 23
+    self.tone = 4
+    self.state = None
+
+    wiringpi.pinMode(self.red, wiringpi.GPIO.OUTPUT)
+    wiringpi.pinMode(self.green, wiringpi.GPIO.OUTPUT)
+
+    wiringpi.pinMode(self.tone, wiringpi.GPIO.OUTPUT)
+    wiringpi.softToneCreate(self.tone)
+
+    self.motor = ServoMotor()
+    self.motor.setup(pin)
+    self.close()
+
+    self.nfc = NfcReader()
+
+  def open(self):
+    self.beep([500,1000])
+    self.motor.rotate(self.motor_pin, 150)
+    self.state = 'Opened'
+    wiringpi.digitalWrite(self.red, 0)
+    wiringpi.digitalWrite(self.green, 1)
+
+  def close(self):
+    self.beep([1000,500])
+    self.motor.rotate(self.motor_pin, 50)
+    self.state = 'Closed'
+    wiringpi.digitalWrite(self.red, 1)
+    wiringpi.digitalWrite(self.green, 0)
+
+  def beep(self, tones):
+    for hz in tones:
+      wiringpi.softToneWrite(self.tone, hz)
+      time.sleep(0.1)
+    wiringpi.softToneWrite(self.tone, 0)
+
+
+  def register_card(self):
+    self.nfc.call(self.nfc.register_card)
+
+  def wait_card(self):
+    self.nfc.info()
+    if self.nfc.is_registered() :
+      if self.state == 'Closed':
+        print "Open !!"
+        self.open()
+      elif self.state == 'Opened':
+        print "Close !!"
+        self.close()
+    else:
+      print "You card is not registerd"
+      self.beep([500,500])
+
+#
+#
+#
+if __name__ == '__main__' :
+  try:
+    id    =  int(sys.argv[1])
+    angle =  int(sys.argv[2])
+
+    servo = ServoMotor() 
+    servo.motor_main(id, angle)
+
+  except:
+    print "Usage: python %s PWM_ID Count" % sys.argv[0]
+
